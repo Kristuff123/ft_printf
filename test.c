3c268def698eba5712a448a3b7b852c98726a86f
Kristuff123 <kaeres.krs@gmail.com>
2024-04-01 15:50:05 +0200

    test

---
+++ test.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "ft_printf.h"
+
+int main() {
+    // Test case 1: Printing a string
+    ft_printf("Hello, world!\n");
+
+    // Test case 2: Printing a character
+    char ch = 'A';
+    int i = 1234567890;
+    ft_printf("The character is %c and int is %d\n", ch, i);
+
+    // Test case 3: Printing a pointer
+    int num = 42;
+    ft_printf("The pointer to num is %p\n", &num);
+
+    // Test case 4: Printing a decimal number
+    ft_printf("The decimal number is %d\n", num);
+    int num2 = -42;
+    // Test case 5: Printing an integer
+    ft_printf("The integer is %i\n", num2);
+
+    // Test case 6: Printing an unsigned decimal number
+    unsigned int u_num = 42;
+    ft_printf("The unsigned decimal number is %u\n", u_num);
+
+    // Test case 7: Printing a number in hexadecimal (lowercase)
+    ft_printf("The hexadecimal number (lowercase) is %x\n", num);
+
+    // Test case 8: Printing a number in hexadecimal (uppercase)
+    ft_printf("The hexadecimal number (uppercase) is %X\n", num);
+
+    // Test case 9: Printing a percent sign
+    ft_printf("The percent sign is %%\n");
+
+    return 0;
+}
