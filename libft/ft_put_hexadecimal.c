9b400eb80056dacdef4f3d61a7ba3d7ba13a35e5
Kristuff123 <kaeres.krs@gmail.com>
2024-04-14 20:06:06 +0200

    hex fixed

--- libft/ft_put_hexadecimal.c
+++ libft/ft_put_hexadecimal.c
@@ -6,7 +6,7 @@
 /*   By: kgraczyk <kgraczyk@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 17:08:27 by krs               #+#    #+#             */
-/*   Updated: 2024/04/11 21:27:04 by kgraczyk         ###   ########.fr       */
+/*   Updated: 2024/04/14 20:05:16 by kgraczyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,17 @@ int	ft_put_hexadecimal(unsigned long n, char format)
 		base = "0123456789abcdef";
 	else
 		base = "0123456789ABCDEF";
-	if (!base || !*base)
-		return (0);
 	i = 0;
 	num = (unsigned int)n;
 	base_length = ft_strlen(base);
 	if (num > (base_length - 1))
+	{
 		i += ft_put_hexadecimal(num / base_length, format);
+		if (i < 0)
+			return (-1);
+	}
 	i += ft_putchar(*(base + (num % base_length)));
+	if (i < 0)
+		return (-1);
 	return (i);
 }
