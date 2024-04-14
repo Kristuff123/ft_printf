ba4e4895e14b6999022566ba914f8301706ee3dc
Kristuff123 <kaeres.krs@gmail.com>
2024-04-14 20:12:59 +0200

    unsigned copilot

--- libft/ft_put_unsigned.c
+++ libft/ft_put_unsigned.c
@@ -6,7 +6,7 @@
 /*   By: kgraczyk <kgraczyk@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 17:13:22 by krs               #+#    #+#             */
-/*   Updated: 2024/04/11 21:24:32 by kgraczyk         ###   ########.fr       */
+/*   Updated: 2024/04/14 20:12:20 by kgraczyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 int	ft_put_unsigned(unsigned long n)
 {
-	int		length;
-	char	*base;
+	int				length;
+	char			*base;
+	unsigned long	base_length;
 
 	base = "0123456789";
-	if (!base || !*base)
-		return (0);
+	base_length = ft_strlen(base);
 	length = 0;
-	if (n > (ft_strlen(base) - 1))
-		length += ft_put_unsigned(n / ft_strlen(base));
-	length += ft_putchar(*(base + (n % ft_strlen(base))));
+	if (n > (base_length - 1))
+		length += ft_put_unsigned(n / base_length);
+	length += ft_putchar(*(base + (n % base_length)));
 	return (length);
 }
